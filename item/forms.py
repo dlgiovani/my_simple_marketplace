cff9d806f1ab6ca045b9881c9c92e9991728f50b
Giovani Drosda Lima <gdrosdalima@gmail.com>
2023-10-14 21:32:55 -0300

    minor adjustment

--- item/forms.py
+++ item/forms.py
@@ -1,13 +1,26 @@
 from django import forms
 from .models import Item
+from django.utils.translation import gettext_lazy as _
 
 INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+class PriceField(forms.FloatField):
+    def to_python(self, value):
+        if value:
+            value = value.replace(',', '.')
+        return super().to_python(value)
+
+    def prepare_value(self, value):
+        if value is not None:
+            return str(value).replace('.', ',')
+        return value
 
 
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('category', 'name', 'description', 'price', 'image_url',)
+        price = PriceField(label=_('Price'), help_text=_('Enter the price using either . or , as cents separator.'))
 
         widgets = {
             'category': forms.Select(attrs={
